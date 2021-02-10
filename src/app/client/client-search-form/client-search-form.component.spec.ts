21345871289ba3617c86d3677342a41195f6607b
Developer.KZ <developer.kz@gmail.com>
2021-02-10 13:42:44 +0600

    search form

---
+++ src/app/client/client-search-form/client-search-form.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ClientSearchFormComponent } from './client-search-form.component';
+
+describe('ClientSearchFormComponent', () => {
+  let component: ClientSearchFormComponent;
+  let fixture: ComponentFixture<ClientSearchFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ClientSearchFormComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ClientSearchFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/client/client-search-form/client-search-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-client-search-form',
+  templateUrl: './client-search-form.component.html',
+  styleUrls: ['./client-search-form.component.css']
+})
+export class ClientSearchFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
