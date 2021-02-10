698d46cab1f9722cb1f206535ad04b18ab0b44e5
Developer.KZ <developer.kz@gmail.com>
2021-02-10 13:42:30 +0600

    search form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchFieldComponent } from './shared/form/search-field/search-field.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialsModule} from './materials/materials.module';
+import { ClientSearchFormComponent } from './client/client-search-form/client-search-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFieldComponent,
+    ClientSearchFormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
