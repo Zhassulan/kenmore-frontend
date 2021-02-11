fa7ef3ebd4b74d6a516608a0b5da8cd4839f2a19
Developer.KZ <developer.kz@gmail.com>
2021-02-11 17:28:32 +0600

    search is done

--- src/app/client/client-data.service.ts
+++ src/app/client/client-data.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {environment} from "../../environments/environment";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {UserStatus} from "./model/user-status";
 import {User} from "./model/user";
 
@@ -20,6 +20,19 @@ export class ClientDataService {
 
   findByName(param: string) {
     return this.http.get<User []>(`${API_URL}/users/name/${param}`);
+  }
+
+  findByEmail(param: string) {
+    return this.http.get<User []>(`${API_URL}/users/email/${param}`);
+  }
+
+  findByBirthdate(param: string) {
+    console.log('Param: ' + param);
+    return this.http.get<User []>(`${API_URL}/users/birthdate/${param}`);
+  }
+
+  findByStatus(param: string) {
+    return this.http.get<User []>(`${API_URL}/users/status/${param}`);
   }
 
 }
--- src/app/client/client-search-form/client-search-form.component.ts
+++ src/app/client/client-search-form/client-search-form.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {UserStatus} from "../model/user-status";
 import {ClientDataService} from "../client-data.service";
 import {User} from "../model/user";
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-client-search-form',
@@ -12,11 +13,13 @@ export class ClientSearchFormComponent implements OnInit {
 
   searchByCategories: string[] = ['name', 'email', 'birthdate', 'status'];
   selectedCategory = this.searchByCategories[0];
+  selectedStatus = '';
   statuses: UserStatus[] = [];
   inputFieldData = '';
   users: User[] = [];
+  public pickerDate = new FormControl(new Date());
 
-  constructor(private clntService : ClientDataService) {
+  constructor(private clntService: ClientDataService) {
   }
 
   ngOnInit(): void {
@@ -31,7 +34,54 @@ export class ClientSearchFormComponent implements OnInit {
 
   findClick() {
     this.users = [];
+    switch (this.selectedCategory) {
+      case this.searchByCategories[0]:
+        this.findByName();
+        break;
+      case this.searchByCategories[1]:
+        this.findByEmail();
+        break;
+      case this.searchByCategories[2]:
+        this.findByBirthdate();
+        break;
+      case this.searchByCategories[3]:
+        this.findByStatus();
+        break;
+    }
+  }
+
+  findByName() {
     this.clntService.findByName(this.inputFieldData).subscribe(
+      data => this.users = data,
+      error => {
+        alert(error.error.message);
+      }
+    );
+  }
+
+  findByEmail() {
+    this.clntService.findByEmail(this.inputFieldData).subscribe(
+      data => this.users = data,
+      error => {
+        alert(error.error.message);
+      }
+    );
+  }
+
+  findByBirthdate() {
+    let dt = new Date(this.pickerDate.value.getTime());
+    let s = `${dt.getDate() < 9 ? '0' + dt.getDate() : dt.getDate()}.${dt.getMonth() < 9 ? '0' + (dt.getMonth() + 1) : dt.getMonth() + 1}.${dt.getFullYear()}`;
+    console.log(s);
+    this.clntService.findByBirthdate(s).subscribe(
+      data => this.users = data,
+      error => {
+        alert(error.error.message);
+      }
+    );
+  }
+
+  findByStatus() {
+    this.clntService.findByStatus(this.selectedStatus).subscribe(
       data => this.users = data,
       error => {
         alert(error.error.message);
